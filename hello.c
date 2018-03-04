224a634f2c08d2af9279411cc41cb7d87b9f5088
garsin <garsinap@gmail.com>
2018-03-04 11:34:55 +0530

    changed c file as user2

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
+void microkernel_sendmsg(char *);
+
 int main(){ printf("Helloworld!\n");
+microkernel_sendmsg("is more portable");
 	return 0;
+}
+
+void microkernel_sendmsg(char *a){ printf("microkernel: %s\n", a);
 }
